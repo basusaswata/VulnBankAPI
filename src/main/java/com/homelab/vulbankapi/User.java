dbb3bb3a8ad8b5c943e1dd704e33be42c422252e
basusaswata <basu.saswata@gmail.com>
2025-03-24 16:33:02 -0500

    aa

--- src/main/java/com/homelab/vulbankapi/User.java
+++ src/main/java/com/homelab/vulbankapi/User.java
@@ -34,10 +34,10 @@ public class User {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
+  
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-}
+} 
