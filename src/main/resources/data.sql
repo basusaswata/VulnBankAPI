3e0fc850d483cf84f728066006248c893e044ff5
basusaswata <basu.saswata@gmail.com>
2025-03-24 16:42:26 -0500

    aa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,10 @@
+
+CREATE TABLE users (
+  id BIGINT PRIMARY KEY,
+  username VARCHAR(255),
+  password VARCHAR(255),
+  role VARCHAR(255),
+  email VARCHAR(255)
+);
 INSERT INTO users (id, username, password, role, email) VALUES (1, 'admin', 'password123', 'ADMIN', 'admin@vulnbank.com');
 INSERT INTO users (id, username, password, role, email) VALUES (2, 'user', 'userpass', 'USER', 'user@vulnbank.com');
